f11bb4c113456530d1a2c1965f69314926c0a79d
Lucasvescovir <lucasvescovir@gmail.com>
2025-09-01 20:47:17 -0300

    feat: aba de metas

--- main.py
+++ main.py
@@ -103,4 +103,37 @@ if file_upload:
             "Evolução 24M Relativa" , 
         ]
         st.line_chart(df_stats[rel_cols])
+
+    with st.expander("Metas"):
+
+        col1, col2 = st.columns(2)
+        data_inicio_meta = col1.date_input("Início da Meta", max_value=df_stats.index.max())
+        data_filtrada = df_stats.index[df_stats.index <= data_inicio_meta][-1]
+
+        gastos = col1.number_input("Gastos fixos", min_value=0.,format="%.2f")
+      
+        salario_bruto = col2.number_input("Salário Bruto", min_value=0., format="%.2f")
+        salario_liquido = col2.number_input("Salário Líquido", min_value=0., format="%.2f")
+        
+        valor_inicio = df_stats.loc[data_filtrada]["Valor"]
+        col1.markdown(f"**Patrimônio no Início da Meta**: R$ {valor_inicio:.2f}")
+
+        col1_pot, col2_pot = st.columns(2)
+        mensal = salario_liquido - gastos
+        anual = mensal * 12
+        with col1_pot.container(border=True):
+
+            st.markdown(f"**Potencial Arrecadação Mês**: R$ {mensal:.2f}")
+        with col2_pot.container(border=True):
+            st.markdown(f"**Potencial Arrecadação Ano**: R$ {anual:.2f}")
+
+        with st.container(border=True):
+            col1_meta, col2_meta = st.columns(2)
+            with col1_meta:
+                meta_estipulada = st.number_input("Meta Estipulada",min_value=-999999999., format="%.2f", value=anual)
+
+            with col2_meta:
+                patrimonio_final = meta_estipulada + valor_inicio
+                st.markdown(f"Patrimônio Estimado Pós Meta:\n\n R$ {patrimonio_final}" )
+
 # Não tem arquivos...
